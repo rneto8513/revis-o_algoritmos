73864c8e100f92dd3da37aee9c0cda381427fe89
Raimundo Abrantes da Silva Neto <rneto8513@gmail.com>
2025-02-04 16:01:43 -0300

    Add files via upload

---
+++ 008.py
@@ -0,0 +1,21 @@
+# Implemente um dicionário “contato” que represente uma pessoa na lista “agenda_telefonica” com as seguintes informações:
+#
+# Nome
+# Telefone
+# Email
+#
+# Crie uma função buscar_contato(agenda_telefonica: list, nome: str) que recebe uma lista de contatos e retorna o
+# contato correspondente ao nome fornecido ou None caso não exista.
+
+def buscar_contato(agenda_telefonica, nome):
+    for contato in agenda_telefonica:
+        if contato["nome"] == nome:
+            return contato
+
+
+agenda_telefonica = [{"nome": "Arthur", "telefone": 12345678, "email": "art@gmail.com"},
+                     {"nome": "Raimundo", "telefone": 55214369, "email": "ramud@gmail.com"}]
+
+
+resultadoDaBusca = buscar_contato(agenda_telefonica, "Raimundo")
+print(resultadoDaBusca)
